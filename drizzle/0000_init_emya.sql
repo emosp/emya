de57aee5eecacb4e49fa57d39b03d3bb4769d612
puzzle <happypuzzle@126.com>
2025-10-12 16:53:11 +0800

    fix sql

--- drizzle/0000_init_emya.sql
+++ drizzle/0000_init_emya.sql
@@ -199,5 +199,5 @@ CREATE INDEX `idx_title` ON `video_list` (`title`);--> statement-breakpoint
 CREATE INDEX `idx_origin_title` ON `video_list` (`origin_title`);--> statement-breakpoint
 CREATE INDEX `idx_media` ON `video_media` (`video_list_id`,`video_season_id`,`video_episode_id`);--> statement-breakpoint
 CREATE INDEX `idx_user_id` ON `video_media` (`user_id`);--> statement-breakpoint
-CREATE INDEX `idx_video_media_id` ON `video_subtitle` (`video_media_id`);
+CREATE INDEX `idx_video_media_id` ON `video_subtitle` (`video_media_id`);--> statement-breakpoint
 CREATE INDEX `idx_user_id` ON `video_subtitle` (`user_id`);
