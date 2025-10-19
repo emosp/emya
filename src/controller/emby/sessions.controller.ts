0314d4fd7cfda0327685a4348d4a1c9fde4b4b12
puzzle <happypuzzle@126.com>
2025-10-19 23:08:43 +0800

    feat session hills ping

--- src/controller/emby/sessions.controller.ts
+++ src/controller/emby/sessions.controller.ts
@@ -5,6 +5,7 @@ import { SessionPlaying } from '@/controller/emby/request.dto'
 import * as db from '@/db'
 import { MySql2Database } from 'drizzle-orm/mysql2'
 import { EmbyService } from '@/controller/emby/emby.service'
+import { formatTimeToEmby } from '@/utils/dayjs'
 
 @Controller(['/emby/sessions'])
 export class SessionsController {
@@ -12,6 +13,43 @@ export class SessionsController {
     @Inject('DB') private model: MySql2Database<typeof db.schema>,
     private EmbyService: EmbyService,
   ) {}
+
+  @Get()
+  async Session() {
+    return [
+      {
+        PlayState: {
+          CanSeek: false,
+          IsPaused: false,
+          IsMuted: false,
+          RepeatMode: 'RepeatNone',
+          SleepTimerMode: 'None',
+          SubtitleOffset: 0,
+          Shuffle: false,
+          PlaybackRate: 1,
+        },
+        AdditionalUsers: [],
+        RemoteEndPoint: 'emya',
+        Protocol: 'HTTP/1.1',
+        PlayableMediaTypes: ['Audio', 'Video'],
+        PlaylistIndex: 0,
+        PlaylistLength: 0,
+        Id: this.EmbyService.Id(),
+        ServerId: this.EmbyService.Id(),
+        UserId: '',
+        UserName: '',
+        Client: '',
+        LastActivityDate: formatTimeToEmby(),
+        DeviceName: '',
+        InternalDeviceId: 0,
+        DeviceId: '',
+        ApplicationVersion: this.EmbyService.Version(),
+        AppIconUrl: 'https://raw.githubusercontent.com/emosp/emya/refs/heads/main/logo.png',
+        SupportedCommands: [],
+        SupportsRemoteControl: false,
+      },
+    ]
+  }
 
   @Post('Playing')
   async SessionPlaying(@Body() body: SessionPlaying, @Res() res: any) {
