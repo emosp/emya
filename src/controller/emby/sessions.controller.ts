15a1b124b36f83d53d0fdaf52c6c863be3600b13
puzzle <happypuzzle@126.com>
2025-12-06 18:07:53 +0800

    fix video playing session

--- src/controller/emby/sessions.controller.ts
+++ src/controller/emby/sessions.controller.ts
@@ -101,7 +101,7 @@ export class SessionsController {
       playing_media_data: {
         id: number
         file_second: number
-      } = JSON.parse((await this.cache.get(playing_media_data_cache_name)) || '[]')
+      } = JSON.parse((await this.cache.get(playing_media_data_cache_name)) || '{}')
 
     if (!playing_media_data.id && video_media_uuid) {
       playing_media_data = (await this.model.query.video_media.findFirst({
